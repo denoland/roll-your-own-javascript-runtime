6c041ace90408df408cfe9d5155329a364090c3c
crowlkats <crowlkats@toaxl.com>
2024-09-26 00:56:14 +0200

    chore: update crates

--- build.rs
+++ build.rs
@@ -3,23 +3,23 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    extension!(runjs, js = ["src/runtime.js",]);
+  extension!(runjs, js = ["src/runtime.js",]);
 
-    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    let snapshot_path = out_dir.join("RUNJS_SNAPSHOT.bin");
+  let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+  let snapshot_path = out_dir.join("RUNJS_SNAPSHOT.bin");
 
-    let snapshot = deno_core::snapshot::create_snapshot(
-        deno_core::snapshot::CreateSnapshotOptions {
-            cargo_manifest_dir: env!("CARGO_MANIFEST_DIR"),
-            startup_snapshot: None,
-            skip_op_registration: false,
-            extensions: vec![runjs::init_ops_and_esm()],
-            with_runtime_cb: None,
-            extension_transpiler: None,
-        },
-        None,
-    )
-    .unwrap();
+  let snapshot = deno_core::snapshot::create_snapshot(
+    deno_core::snapshot::CreateSnapshotOptions {
+      cargo_manifest_dir: env!("CARGO_MANIFEST_DIR"),
+      startup_snapshot: None,
+      skip_op_registration: false,
+      extensions: vec![runjs::init_ops_and_esm()],
+      with_runtime_cb: None,
+      extension_transpiler: None,
+    },
+    None,
+  )
+  .unwrap();
 
-    std::fs::write(snapshot_path, snapshot.output).unwrap();
+  std::fs::write(snapshot_path, snapshot.output).unwrap();
 }
